a74ad114ce4564784cf28a9f2e3e969aafe84add
KozaX <55134615+KozaX@users.noreply.github.com>
2020-08-04 13:42:08 +0200

    finish profile editing business logic

--- routes/profile.js
+++ routes/profile.js
@@ -1,15 +1,47 @@
 'use strict';
 
 const User = require('./../models/user');
+const Wish = require('./../models/wish');
 
 const { Router } = require('express');
 const router = new Router();
 
+const routeGuard = require('./../middleware/route-guard');
+
 router.get('/:id', (req, res, next) => {
+  const id = req.params.id;
+  let user;
+  User.findById(id)
+    .then(data => {
+      user = data;
+      return Wish.find({ creator: id });
+    })
+    .then(wish => {
+      console.log(wish);
+      res.render('profile/display', { user, wish });
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+router.get('/:id/edit', routeGuard, (req, res, next) => {
   const id = req.params.id;
   User.findById(id)
     .then(data => {
-      res.render('profile/display', { data });
+      res.render('profile/edit', { data });
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+router.post('/:id/edit', routeGuard, (req, res, next) => {
+  const id = req.params.id;
+  const { name, email } = req.body;
+  User.findByIdAndUpdate(id, { name, email })
+    .then(() => {
+      res.redirect('/home');
     })
     .catch(err => {
       next(err);
